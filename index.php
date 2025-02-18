754d8fd8c910b707f12f0d3cb8fad75279ca3f40
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2025-02-18 14:44:20 -0800

    did the header thing

--- index.php
+++ index.php
@@ -15,8 +15,28 @@
 <body>
 <header class="reserve-header">
     <div class="HETBUREAU-LOGO">
-<img src="HETBUREAU-LOGO-ZWART.webp" alt="HETBUREAU-LOGO-ZWART">
-</div>
+        <img src="Layer 2.png" alt="HETBUREAU-LOGO-ZWART">
+    </div>
+    <div class="time-date-info">
+        <div class="label" id="time-label">Tijd:</div>
+        <div class="current" id="time-current">
+            <?php
+            echo date("H:i");
+            ?>
+        </div>
+        <div class="label" id="date-label">Datum:</div>
+        <div class="current" id="date-current">
+            <?php
+            echo date("d-m-Y");
+            ?>
+        </div>
+    </div>
+    <div class="qr">
+        <p>SCAN MIJ!</p>
+        <div class="qr-box">
+            <img src="qr-code.png" alt="qr-code">
+        </div>
+    </div>
 </header>
 <div class="reserve-vandaag">
     <div class="reserve-vandaag-tekst">
