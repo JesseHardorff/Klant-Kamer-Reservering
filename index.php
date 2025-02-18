99f16c3058ecb61b3277f548604458ec03849763
JesseHardorff <144345868+JesseHardorff@users.noreply.github.com>
2025-02-18 10:32:13 +0100

    update basics

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Klant-Kamer-Reservering</title>
+  
+</head>
+<body>
+<header class="reserve-header">
+
+</header>
+<div class="reserve-vandaag">
+</div>
+<div class="reserve-rest">
+</div>
+
+</body>
+<!-- <script src="assets/js/bla.js"></script> -->
+</html>
