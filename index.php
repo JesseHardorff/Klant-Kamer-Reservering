184f45bb7784d875f474385af26f6839b3b83aa3
JesseHardorff <144345868+JesseHardorff@users.noreply.github.com>
2025-02-18 11:01:33 +0100

    reserve vandaag layout

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
 
+
     <title>Klant-Kamer-Reservering</title>
+    <link rel="icon" type="image/x-icon" href="BUREAU-LOGO.ico">
   
 </head>
 <body>
@@ -14,7 +16,13 @@
 
 </header>
 <div class="reserve-vandaag">
+    <div class="reserve-vandaag-tekst">
+    </div>
+    <div class="reserveringen">
+    </div>
 </div>
+
+
 <div class="reserve-rest">
 </div>
 
