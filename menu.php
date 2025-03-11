9c1854c1345410743cbe7e37994afe5cb2460708
JesseHardorff <144345868+JesseHardorff@users.noreply.github.com>
2025-03-11 10:41:32 +0100

    menu layout

---
+++ menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Klant-Kamer-Reservering Menu</title>
+    <link rel="stylesheet" href="assets/css/menu.css">
+</head>
+
+<body>
+    <div class="menu">
+    </div>
+</body>
+
+</html>
