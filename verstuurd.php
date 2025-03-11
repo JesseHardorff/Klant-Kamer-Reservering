b850ce58c02f6c4d78692e94f46ff778677ddc0e
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2025-03-11 14:02:41 -0700

    done sorta

--- verstuurd.php
+++ verstuurd.php
@@ -19,9 +19,23 @@
         </div>
         <div class="header-text-wrapper"><h1 class="header-text">Verstuurd</h1><p>als 230838</p></div>
     </div>
-    <div class="status">
+    <div class="status failed">
         <h2>Reservering verstuurd</h2>
         <p>Uw reservering is verstuurd naar de beheerder</p>
         <button class="submit-button">TERUG NAAR HOMEPAGE</button>
+    </div>
+    <div class="data">
+        <h3>Lokaal</h3>
+        <p>W002a</p>
+        <h3>Datum</h3>
+        <p>11-03-2025</p>
+        <h3>Start tijd</h3>
+        <p>12:15</p>
+        <h3>Eind tijd</h3>
+        <p>13:00</p>
+        <h3>Klant</h3>
+        <p>Graffr</p>
+        <h3>Type</h3>
+        <p>Klant Gesprek</p>
     </div>
 </body>
