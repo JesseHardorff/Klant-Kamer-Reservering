e9a571717fa5a1ae901471ddb46cc884dbce9487
JesseHardorff <144345868+JesseHardorff@users.noreply.github.com>
2025-03-11 12:10:46 +0100

    Menu.php klaar

--- menu.php
+++ menu.php
@@ -6,11 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Klant-Kamer-Reservering Menu</title>
     <link rel="stylesheet" href="assets/css/menu.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
     <div class="menu">
-    </div>
+        <div class="logo-container">
+            <img class="logo" src="Layer 2.png" alt="HETBUREAU-LOGO-ZWART">
+        </div>
+        <div class="student-number-container">
+            <div class="student-number">
+                <p>000000</p>
+                <!-- student numemr/naam uit database via php -->
+            </div>
+            <div class="student-number-text">
+                <p>User</p>
+            </div>
+        </div>
+        <div class="menu-buttons">
+            <div class="button-reserveringen">
+                <p>Reserveringen</p>
+            </div>
+            <div class="button-instellingen">
+                <p>Instellingen</p>
+            </div>
+        </div>
 </body>
 
 </html>
