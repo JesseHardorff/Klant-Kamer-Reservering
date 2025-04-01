3309a8116deac072933c50c4b1d2b8ae892fac50
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2025-04-01 10:06:44 +0200

    add back menu

---
+++ menu.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Klant-Kamer-Reservering Menu</title>
+    <link rel="stylesheet" href="assets/css/menu.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <div class="menu">
+        <div class="logo-container">
+            <img class="logo" src="Layer 2.png" alt="HETBUREAU-LOGO-ZWART">
+        </div>
+        <div class="student-number-container">
+            <div class="student-number">
+                <p>000000</p>
+                <!-- student numemr/naam uit database via php -->
+            </div>
+            <div class="student-number-text">
+                <p>User</p>
+            </div>
+        </div>
+        <div class="menu-buttons">
+            <a class="button-reserveringen" href="reserve.php">
+                <p>Reserveren</p>
+            </a>
+            <a class="button-instellingen" href="#">
+                <p>Reserveringen</p>
+            </a>
+        </div>
+</body>
+
+</html>
