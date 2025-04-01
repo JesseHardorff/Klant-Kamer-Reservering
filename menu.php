abff4da253d2f217da83bc7c11919f273b866d50
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2025-04-01 14:56:35 +0200

    menu update

--- menu.php
+++ menu.php
@@ -5,32 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Klant-Kamer-Reservering Menu</title>
-    <link rel="stylesheet" href="assets/css/menu.css">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/menu.css">
+
+    <link rel="icon" type="image/x-icon" href="BUREAU-LOGO.ico">
 </head>
 
 <body>
-    <div class="menu">
-        <div class="logo-container">
-            <img class="logo" src="Layer 2.png" alt="HETBUREAU-LOGO-ZWART">
+    <div class="logo-container">
+        <img class="logo" src="Layer 2.png" alt="HETBUREAU-LOGO-ZWART">
+    </div>
+    <div class="student-number-container">
+        <div class="student-number">
+            <p>000000</p>
+            <!-- student numemr/naam uit database via php -->
         </div>
-        <div class="student-number-container">
-            <div class="student-number">
-                <p>000000</p>
-                <!-- student numemr/naam uit database via php -->
-            </div>
-            <div class="student-number-text">
-                <p>User</p>
-            </div>
+        <div class="student-number-text">
+            <p>Naam</p>
         </div>
-        <div class="menu-buttons">
-            <a class="button-reserveringen" href="reserve.php">
-                <p>Reserveren</p>
-            </a>
-            <a class="button-instellingen" href="#">
-                <p>Reserveringen</p>
-            </a>
-        </div>
+    </div>
+    <div class="menu-buttons">
+        <a class="button-reserveringen" href="reserve.php">
+            <p>Reserveren</p>
+        </a>
+        <a class="button-instellingen" href="#">
+            <p>Reserveringen</p>
+        </a>
+    </div>
 </body>
 
 </html>
